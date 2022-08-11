b9f0c5cfe7cc61324c0364833a47ca2ee0b3f42a
Ricardo Lüders <rluders@redhat.com>
2022-08-11 14:15:13 +0200

    feat(migration): adding classroom missing columns

--- db/migrations/1660203374_clasrooms.up.sql
+++ db/migrations/1660203374_clasrooms.up.sql
@@ -10,8 +10,11 @@ CREATE TABLE classrooms
     name            varchar         NOT NULL,
     description     text,
     format          varchar         DEFAULT 'online',
+    can_subscribe   bool            DEFAULT false,
     subject_id      uuid,
     course_id       uuid            NOT NULL,
+    starts_at       timestamp       DEFAULT now() NOT NULL,
+    ends_at         timestamp,
     created_at      timestamp       DEFAULT now() NOT NULL,
     updated_at      timestamp       DEFAULT now() NOT NULL,
     deleted_at      timestamp
